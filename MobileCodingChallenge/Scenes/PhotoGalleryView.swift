4ecb959a608127001650ef8f869925adf784aac8
bLajen <batuhanballi34@gmail.com>
2024-01-08 18:39:48 -0500

    Add PhotoGalleryView struct

---
+++ MobileCodingChallenge/Scenes/PhotoGalleryView.swift
@@ -0,0 +1,16 @@
+//
+//  PhotoGalleryView.swift
+//  MobileCodingChallenge
+//
+//  Created by Batuhan Ballı on 2024-01-08.
+//
+
+import SwiftUI
+
+struct PhotoGalleryView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
