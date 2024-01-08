eb4a04ff6bd9c7557194d896d8bce0e059536f9a
bLajen <batuhanballi34@gmail.com>
2024-01-08 18:09:31 -0500

    Initial Commit

---
+++ MobileCodingChallenge/MobileCodingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileCodingChallengeApp.swift
+//  MobileCodingChallenge
+//
+//  Created by Batuhan Ballı on 2024-01-08.
+//
+
+import SwiftUI
+
+@main
+struct MobileCodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
