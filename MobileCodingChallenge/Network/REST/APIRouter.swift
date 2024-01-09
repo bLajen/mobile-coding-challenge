d03cf80b41557954c735487da8dfb2d5a4ffae18
bLajen <batuhanballi34@gmail.com>
2024-01-09 16:36:47 -0500

    Update APIRouter - Add request function

--- MobileCodingChallenge/Network/REST/APIRouter.swift
+++ MobileCodingChallenge/Network/REST/APIRouter.swift
@@ -9,6 +9,28 @@ import Alamofire
 import Foundation
 
 protocol APIRouter: URLRequestConvertible {
+    var method: HTTPMethod { get }
+    var path: String { get }
+    var parameters: Parameters? { get }
+    
+    func request(url: URL) throws -> URLRequest
 }
 
+extension APIRouter {
+    func request(url: URL) throws -> URLRequest {
+        var urlRequest = URLRequest(url: url.appendingPathComponent(path))
+        urlRequest.httpMethod = method.rawValue
+        
+        let encoding: ParameterEncoding = {
+            switch method {
+            case .get:
+                return URLEncoding.default
+            default:
+                return JSONEncoding.default
+            }
+        }()
+        
+        return try encoding.encode(urlRequest, with: parameters)
+    }
+}
 
