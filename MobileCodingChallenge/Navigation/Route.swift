57549df7d54c1487816adf389d6b5ee73868c901
bLajen <batuhanballi34@gmail.com>
2024-01-11 14:44:48 -0500

    Add PhotoGalleryDetailCoordinator - Update Route

--- MobileCodingChallenge/Navigation/Route.swift
+++ MobileCodingChallenge/Navigation/Route.swift
@@ -9,12 +9,27 @@ import SwiftUI
 
 enum Route {
     case gallery
+    case detail(viewModel: PhotoGalleryDetailViewModel)
     
     @ViewBuilder
     func build() -> some View {
         switch self {
         case .gallery:
             PhotoGalleryCoordinator().start()
+        case let .detail(viewModel):
+            PhotoGalleryDetailCoordinator(photoGalleryDetailViewModel: viewModel).start()
         }
+    }
+}
+
+extension Route: Hashable, Identifiable {
+    var id: Int { UUID().hashValue }
+    
+    static func == (lhs: Route, rhs: Route) -> Bool {
+        lhs.id == rhs.id
+    }
+    
+    func hash(into myhasher: inout Hasher) {
+        myhasher.combine(id)
     }
 }
--- MobileCodingChallenge/Scenes/PhotoGalleryDetail/PhotoGalleryDetailCoordinator.swift
+++ MobileCodingChallenge/Scenes/PhotoGalleryDetail/PhotoGalleryDetailCoordinator.swift
@@ -5,4 +5,19 @@
 //  Created by Batuhan Ballı on 2024-01-11.
 //
 
-import Foundation
+import SwiftUI
+
+final class PhotoGalleryDetailCoordinator: Coordinator {
+    @Published var path = NavigationPath()
+    
+    private let photoGalleryDetailViewModel: PhotoGalleryDetailViewModel
+    
+    init(photoGalleryDetailViewModel: PhotoGalleryDetailViewModel) {
+        self.photoGalleryDetailViewModel = photoGalleryDetailViewModel
+    }
+    
+    @ViewBuilder
+    func start() -> some View {
+        PhotoGalleryDetailView(viewModel: photoGalleryDetailViewModel)
+    }
+}
