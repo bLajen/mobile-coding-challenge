eb7047e5dd0afaf6e65918ecbceb17f122512420
bLajen <batuhanballi34@gmail.com>
2024-01-11 00:22:24 -0500

    Update CoreCollectionView with UIViewRepresentable

--- MobileCodingChallenge/CollectionView/CollectionView.swift
+++ MobileCodingChallenge/CollectionView/CollectionView.swift
@@ -11,6 +11,7 @@ import Combine
 struct CollectionView<SectionType, ItemType>
 where SectionType: Hashable & Sendable,
       ItemType: Hashable & Sendable {
+    typealias BaseCollectionView = CoreCollectionView<SectionType, ItemType>
     typealias DataSource = UICollectionViewDiffableDataSource<SectionType, ItemType>
     typealias Snapshot = NSDiffableDataSourceSnapshot<SectionType, ItemType>
     
@@ -27,4 +28,46 @@ where SectionType: Hashable & Sendable,
         self.cellProvider = cellProvider
     }
     
+}
+
+// MARK: UIViewRepresentable
+
+extension CollectionView: UIViewRepresentable {
+    func makeUIView(context: Context) -> BaseCollectionView {
+        let collectionView = BaseCollectionView(frame: .zero,
+                                                collectionViewLayout: UICollectionViewLayout(),
+                                                cellProvider: cellProvider)
+        return collectionView
+    }
+    
+    func updateUIView(_ uiView: BaseCollectionView,
+                      context: Context) {
+        uiView.apply(snapshot,
+                     animatingDifferences: false)
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(photos: $photos)
+    }
+    
+    final class Coordinator: NSObject {
+        @Binding private var photos: [PhotoResponse]
+        
+        init(photos: Binding<[PhotoResponse]>) {
+            self._photos = photos
+        }
+        
+        func collectionView(_ collectionView: UICollectionView,
+                            heightForPhotoAtIndexPath indexPath: IndexPath,
+                            cellWidth: CGFloat) -> CGFloat {
+            let item = photos[indexPath.row]
+            
+            guard let width = item.width,
+                  let height = item.height else { return 0 }
+            
+            let aspectRatio = CGFloat(width) / CGFloat(height)
+            
+            return cellWidth / aspectRatio
+        }
+    }
 }
